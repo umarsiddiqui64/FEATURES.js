ebecd1255a2f573e45b7517c9bd9918aac640ecc
Umar Siddiqui <umarsiddiqui64@gmail.com>
2024-12-07 20:00:59 +0530

    Implemented merging and updating nested objects with spread operator

---
+++ mergeNestedObjects.js
@@ -0,0 +1,29 @@
+const profile = {
+    name: "Charlie",
+    age: 29,
+    address: {
+      city: "San Francisco",
+      zipcode: "94101",
+    },
+  };
+  
+  const updates = {
+    age: 30,
+    address: {
+      zipcode: "94109",
+      country: "USA",
+    },
+  };
+  
+  
+  const mergedProfile = {
+    ...profile,
+    age: updates.age, 
+    address: {
+      ...profile.address, 
+      ...updates.address, 
+    },
+  };
+  
+  console.log(mergedProfile);
+  
